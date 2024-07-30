4ac2f23485723534986e8a853cf73b763ca1afd3
Moulaali <moulaalihujare@gmail.com>
2024-07-30 16:24:23 +0530

    New Version 1.0

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("nav");
+
+window.onscroll= function() {
+
+  if (window.pageXOffset >= offsetTop){
+
+    navbar.classList.add("sticky");
+  }
+  else{
+
+    navbar.classList.remove(sticky);
+  }
+}
